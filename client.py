e7729c1c9e67a75231442ab29f86493cb31557e8
Saahitya <saahitya.e@gmail.com>
2018-11-05 19:13:13 +0530

    Barebones working, modified some stuff

--- client.py
+++ client.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python3
 
+import socket
 
 from threading import Thread
-import socket
 from os import getpid
 
 PID=getpid()
 TCP_PORT=6000
 TCP_HOST='127.0.0.1'
-BUFFERSIZE=1000
 QUESTION_COUNTER=0
 SEND_STRING=""
 
 #Creating socket object
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-
+s.setblocking(True)
 #Initial data sent to register as a client
 NAME=input("Enter your name :")
 
+'''
 def check_connection():
     while(True):
         try:
@@ -26,21 +26,36 @@ def check_connection():
             s.send(bytes(SEND_STRING,'utf-8'))
             break
         except:
-            print("Trying to establish connection again.")
-            s.connect((TCP_HOST,TCP_PORT))
+            #print("Trying to establish connection again.")
+            try:
+                s.connect((TCP_HOST,TCP_PORT))
+                break
+            except:
+                pass
+                #print("Not connecting trying again")
+'''
 
 def establish_connection():
-	s.connect((TCP_HOST,TCP_PORT))
-
+    while(True):
+        try:
+            print("Established")
+            s.connect((TCP_HOST,TCP_PORT))
+            break
+        except:
+            print("Connection not established")
 #In case you want some timeout ask server to send
 #t=s.recv(BUFFERSIZE)
 
 establish_connection()
-
+s.sendall(NAME.encode())
+s.recv(1024)
+print("Hello this is me")
 while(QUESTION_COUNTER<3):
-    check_connection()
-    q=s.recv(BUFFERSIZE)
-    print(q.decode('utf-8'))
-    QUESTION_COUNTER+=1
+    #check_connection()
+    q=s.recv(1024)
+    #print("My dear friends")
+    s.sendall("why the hell are you doing?".encode())
+    print(q.decode())
+    #QUESTION_COUNTER+=1
 
 s.close()
--- server.py
+++ server.py
@@ -16,29 +16,35 @@ quiz_end = False
 HOST = '127.0.0.1'
 PORT = 6000
 
+started_quiz = False
+tot_no_of_questions = 0
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setblocking(True)
 
 def client_conn(client, addr, player_no):
     print("This is me")
     ques_no = 0
     name = client.recv(1024)
+    print(name.decode())
+    name = "saahitya"
     names.append(name)
     while(not started_quiz):
-        print("T")
         pass
     print("Hi there")
     while(ques_no < tot_no_of_questions):
+        print("This is a new question")
         while(ques_no >= no_of_questions_entered):
             pass
         while(ques_no < no_of_questions_entered):
-            print("This is a new question")
+            print("In while loop")
             t0 = time.time()
-            client.send(question[ques_no])
+            client.send((questions[ques_no]).encode())
             ans = client.recv(1024)
+            print(ans.decode())
             t1 = time.time()
-            answers.append((t1 - t0, player_no, t1 - t0, question_no, ans))
+            #answers.append((t1 - t0, player_no, t1 - t0, question_no, ans))
             ques_no += 1
-        
     client.close()
 
 
@@ -51,6 +57,7 @@ while True:
     print("Waiting for players to join ...")
     conn, addr = s.accept()
     t = Thread(target = client_conn, args=(conn, addr, no_of_players+1))
+    t.start()
     no_of_players += 1
     print(str(no_of_players) + " players have joined")
     print("Press Y(Yes) to wait for more players or N(No) to start the Quiz")
@@ -58,10 +65,11 @@ while True:
     if(choice == "N"):
         break
 
-started_quiz = True
 
 print("Enter no of questions to be entered")
 tot_no_of_questions = int(input())
+
+started_quiz = True
 
 no_of_questions_entered = 0
 while(no_of_questions_entered < tot_no_of_questions):
@@ -69,5 +77,5 @@ while(no_of_questions_entered < tot_no_of_questions):
     question = input()
     questions.append(question)
     no_of_questions_entered += 1
-
+print("This is the end")
 ended_quiz = True
